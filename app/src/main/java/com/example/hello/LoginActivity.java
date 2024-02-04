a547b70dbedd23d8abb9fba95becbb77f2db192a
SeaOreos <145692907+SeaOreos@users.noreply.github.com>
2024-02-04 18:44:04 -0500

    i love bts 4EVER

--- app/src/main/java/com/example/hello/LoginActivity.java
+++ app/src/main/java/com/example/hello/LoginActivity.java
@@ -3,12 +3,115 @@ package com.example.hello;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.ValueEventListener;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText loginUsername, loginPassword;
+    Button loginButton;
+    TextView signupRedirectText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginUsername = findViewById(R.id.login_username);
+        loginPassword = findViewById(R.id.login_password);
+        signupRedirectText = findViewById(R.id.signupRedirectText);
+        loginButton = findViewById(R.id.login_button);
+
+        loginButton.setOnClickListenr(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!validate Username () | !validate Password()){
+
+                } else {
+                    checkuser();
+                }
+            }
+
+    });
+
+        signupRedirectText.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent( packageContext: LoginActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+
+    });
+    }
+
+
+    public Boolean validateUsername(){
+        String val = loginUsername.getText().toString();
+        if (val.isEmpty()){
+            loginUsername.setError("Username cannot be empty");
+            return false;
+
+
+        } else {
+            loginUsername.setError(null);
+            return true;
+        }
+
+    }
+
+
+    public Boolean validatePassword(){
+        String val = loginPassword.getText().toString();
+        if (val.isEmpty()){
+            loginPassword.setError("Password cannot be empty");
+            return false;
+
+
+        } else {
+            loginPassword.setError(null);
+            return true;
+        }
+
+    }
+
+    public_void checkUser(){
+        String userUsername = loginUsername.getText().toString().trim();
+        String userPassword = loginPassword.getText().toString().trim();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference( path: "users");
+        Query checkUserDatabase = reference.orderByChild("username").equalTo(userUsername);
+
+        checkUserDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot){
+
+                if (snapshot.exists()){
+                    loginUsername.setError(null);
+                    String passwordFromDB = snapshot.child(userUsername).child("password").getValue(String.class);
+
+                    if (passwordFromDB.equals(userPassword)){
+                        loginUsername.setError(null);
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        startActivity(intent);
+                    } else {
+                        loginPassword.setError("Invalid Credentials");
+                        loginPassword.requestFocus();
+                    }
+
+                } else {
+                    loginUsername.setError("User does not exist");
+                    loginUsername.requestFocus();
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+
+    });
     }
 }
--- app/src/main/java/com/example/hello/SignupActivity.java
+++ app/src/main/java/com/example/hello/SignupActivity.java
@@ -6,9 +6,55 @@ import android.os.Bundle;
 
 public class SignupActivity extends AppCompatActivity {
 
+    EditText signupName, signupEmail, signupUsername, signupPassword;
+    TextView loginRedirectText;
+    Button signupButton;
+    FirebaseDatabase database;
+    DatabaseRegeference reference;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        signupName = findViewById(R.id.signup_name);
+        signupEmail = findViewById(R.id.signup_email);
+        signupUsername = findViewById(R.id.signup_username);
+        signupPassword = findViewById(R.id.signup_password);
+        signupButton = findViewById(R.id.signup_button);
+        loginRedirectText = findViewById(R.id.loginRedirectText);
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                database = FirebaseDatabase.getInstance();
+                reference = database.getReference(path:"users");
+
+                String name = signupName.getText().toString();
+                String email = signupEmail.getText().toString();
+                String username = signupUsername.getText().toString();
+                String password = signupPassword.getText().toString();
+
+                HelperClass helperClass = new HelperClass(name, email, username, password);
+                reference.child(username).setValue(helperClass);
+
+                Toast.makeText(context:SignupActivity.this, text
+                "You have signed up successfully!!!", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(packageContent:SignupActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+
+        });
+        loginRedirectText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                Intent intent = new Intent(packageContent:SignupActivity.this, LoginActivity.class);
+                startActivity(intent);
+
+            }
+
+
+        });
     }
 }
